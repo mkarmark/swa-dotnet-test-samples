7136a71e4c6947cad5620eba78100f927ac60088
mkarmark <mikarmar@microsoft.com>
2023-08-22 22:32:28 -0700

    Update HttpTrigger.cs

--- functions/dotnet6-inproc/HttpTrigger.cs
+++ functions/dotnet6-inproc/HttpTrigger.cs
@@ -31,5 +31,21 @@ namespace dotnet6_inproc
 
             return new OkObjectResult(responseMessage);
         }
+
+        [FunctionName("GetCsv")]
+        public static async Task<IActionResult> RunGetCsvAsync(
+                    [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req)
+        {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write("hello world!");
+            writer.Flush();
+            stream.Position = 0;
+        
+            return new FileStreamResult(stream, "text/csv")
+            {
+                FileDownloadName = $"Sample-{DateTime.Today.ToString("yyyy-MM-dd")}.csv",
+            };
+        }
     }
 }
